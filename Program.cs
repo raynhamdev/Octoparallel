fcbaec7493274766748f42e0ef63e57bc3d4aac2
Ray Nham <ray.nham@octopus.com>
2024-08-28 16:32:53 +1000

    wip

--- Program.cs
+++ Program.cs
@@ -2,22 +2,87 @@
 
 class OctoParallel
 {
-    public async Task Execute(OctoParallelState state, int maxParallelism)
+    public OctoParallel(OctoParallelState state)
     {
-        var nextItems = state.GetNextItems(maxParallelism);
+        State = state;
+    }
+
+    public OctoParallelState State { get; set; }
+    public async Task Execute(int maxParallelism)
+    {
+        var nextItems = State.GetNextItems(maxParallelism);
         if (nextItems.Count == 0)
         {
             return;
         }
         
         var tasks = nextItems.Select(item => Task.Run(() => item.Execute()));
-        state.WorkItems.Remove(nextItems);
+        State.WorkItems.Remove(nextItems);
         
         return Task.WhenAll(tasks);
     }
 }
 
-internal class OctoParallelState
+public class OctoParallelState
 {
     public List<WorkItem> WorkItems { get; set; }
+}
+
+class ControllerBase
+{
+    public Controller(OctoParallel octoParallel, OctoParallelState state)
+    {
+        OctoParallel = octoParallel;
+        State = state;
+    }
+
+    public OctoParallel OctoParallel { get; set; }
+    public OctoParallelState State { get; set; }
+
+    public async Task ExecuteBase()
+    {
+        OctoParallel.Execute();
+    }
+
+    public async Task PreExecute()
+    {
+        State.WorkItems = new List<WorkItem>();
+    }
+}
+
+class Controller : ControllerBase
+{
+    public Controller(OctoParallel octoParallel, OctoParallelState state) : base(octoParallel, state)
+    {
+    }
+
+    public async Task Execute()
+    {
+        await PreExecute();
+        await ExecuteBase();
+    }
+}
+
+public class Execution
+{
+    public OctoParallelState OctoParallelState { get; set; }
+    
+    public async Task Start()
+    {
+        var octoParallel = new OctoParallel(OctoParallelState);
+        var controller = new Controller(octoParallel, OctoParallelState);
+        await controller.Execute();
+    }
+
+    public async Task StatusCheck(bool isComplete)
+    {
+        if (isComplete)
+        {
+            var octoParallel = new OctoParallel(OctoParallelState);
+            var controller = new Controller(octoParallel, OctoParallelState);
+
+            octoParallel.Complete(workItemId);
+            controller.Execute();
+        }
+    }
 }
