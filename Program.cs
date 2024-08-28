622c850a5209fa04c7a99bc758249180f26c0502
Ray Nham <ray.nham@octopus.com>
2024-08-28 16:18:32 +1000

    wip

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class OctoParallel
+{
+    public async Task Execute(OctoParallelState state, int maxParallelism)
+    {
+        var nextItems = state.GetNextItems(maxParallelism);
+        if (nextItems.Count == 0)
+        {
+            return;
+        }
+        
+        var tasks = nextItems.Select(item => Task.Run(() => item.Execute()));
+        state.WorkItems.Remove(nextItems);
+        
+        return Task.WhenAll(tasks);
+    }
+}
+
+internal class OctoParallelState
+{
+    public List<WorkItem> WorkItems { get; set; }
+}
